8c086280c0c1d417d41188027e9572ef7ad82dd6
Alejandro-121 <77085698+Alejandro-121@users.noreply.github.com>
2026-06-09 09:53:15 +0200

    Refactor launch_snpeff.sh for improved processing

--- snpeff/launch_snpeff.sh
+++ snpeff/launch_snpeff.sh
@@ -1,35 +1,25 @@
 #!/bin/bash
-
-# load the conda env
-# conda activate my_snpeff
-
 mkdir -p out_vcf
 
-parallel bgzip {} ::: *vcf
- 
-# exit if conda fails to load
-for file in *.gz
-do
-	# get prefix
-	prefix_0=$(basename "$file" .gz)
-	prefix=$(basename "$prefix_0" .vcf)
+parallel bgzip {} ::: *.vcf
+wait
 
-	echo this is the prefix "$prefix"
-	tabix -p vcf ${file}.vcf.gz
+for file in *.vcf.gz; do
+    prefix=$(basename "$file" .vcf.gz)
+    echo "Processing: $prefix"
 
-	# create a dir to save out
-	mkdir ${prefix}_dir
- 
-	# launch snpEF           database     sample   
-	java -jar snpEff.jar -v R64-1-1_sgd ${prefix}.vcf.gz > anotated_${prefix}.vcf -stats ${prefix}.txt -csvStats ${prefix}.csv
-	
+    tabix -p vcf "$file"                          
+    mkdir -p "${prefix}_dir"
 
-	# copy the file to out dir
-	cp anotated_${prefix}.vcf out_vcf
-	mv anotated_${prefix}.vcf ${prefix}_dir 
-	mv ${prefix}.txt ${prefix}_dir
-	mv ${prefix}.csv ${prefix}_dir
-	mv ${prefix}.genes.txt ${prefix}_dir
-	# mv the compresed file also
-	mv ${prefix}.vcf.gz ${prefix}_dir
+    java -jar snpEff.jar -v R64-1-1_sgd \    
+        -stats "${prefix}.txt" \
+        -csvStats "${prefix}.csv" \
+        "$file" > "anotated_${prefix}.vcf"
+
+    cp "anotated_${prefix}.vcf" out_vcf/
+    mv "anotated_${prefix}.vcf" "${prefix}_dir/"
+    mv "${prefix}.txt"       "${prefix}_dir/"
+    mv "${prefix}.csv"       "${prefix}_dir/"
+    mv "${prefix}.genes.txt" "${prefix}_dir/"
+    mv "$file"               "${prefix}_dir/"
 done
